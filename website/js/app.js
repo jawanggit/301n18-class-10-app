0e91e3efa82aa00175df950f50455e526db152a7
James Wang <jawang.um@gmail.com>
2020-06-30 20:21:12 -0700

    got server to talke to app.js

--- website/js/app.js
+++ website/js/app.js
@@ -1,21 +1,37 @@
 'use strict';
 
+let API = 'http://localhost:3000';
+
 function renderThings(list) {
-  const template = $('#thingsToDoTemplate').html;
-  const container = $('.things');
+  const template = $('#thingsToDoTemplate').html();
+  const container = $('#things');
   list.forEach(item => {
-    let newItemHTML = Mustache.render(item,template)
+    let newItemHTML = Mustache.render(template, item)
     container.append(newItemHTML);
   });
 }
 
 function showThingsToDo() {
 
-  let things = [
-    { thing: 'watch tv' },
-    { thing: 'take a nap' },
-  ];
-  renderThings(things);
+  const ajaxSettings = {
+    method: 'get',
+    dataType: 'json'
+  };
+
+  $.ajax(`${API}/todo`, ajaxSettings)
+    .then(things => {
+      renderThings(things);
+    });
+
+
+  // let things = [
+  //   { task: 'watch tv' },
+  //   { task: 'take a nap' },
+  // ];
+
+
+
+  
 
 }
 
